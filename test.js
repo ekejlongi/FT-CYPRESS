9d671f49e78e443ca4b5a695679b0d56ce4f7d76
Adam Dumanski <adam.dumanski@gmail.com>
2024-06-08 15:13:15 +0200

    usuniecie katalogow z e2e cypress

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log('this is my first log')
+describe ('my first test', () => {
+    beforeEach(() => {
+        cy.visit('https://fabrykatestow.pl/')
+    })
+it ('test1', () => {
+    cy.get('span').contains('ZAPISZ MNIE NA NEWSLETTER!').click()
+})
+    });
+
+
+  //  <span class="elementor-button-text">ZAPISZ MNIE NA NEWSLETTER!</span>
